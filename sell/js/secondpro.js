5e124ff9b83964668025d4f3abe478760d7ca473
jakehao <jianzong@klook.com>
2014-11-06 10:13:18 +0800

    update items.

--- sell/js/secondpro.js
+++ sell/js/secondpro.js
@@ -1,18 +1,11 @@
 var items = [ 
 { name: "macbook",
   title: "Macbook Pro 13寸 MC374", 
-  price: "2900.00",
+  price: "已出",
   original_price: "8500.00",
   new: "七成新",
   date: "2014-11-4",
   desc: "最近换了rmbp，所以出这部自用开发机。2010年港行，功能无损、外观成色垃圾，机器右侧很久之前曾经摔了一个坑出来。已经加装了128GB的镁光ssd，与原来的256GBSATA硬盘共同使用。"}, 
-{ name: "kindle",
-  title: "Kindle 4 送官方带灯皮套", 
-  price: "250.00",
-  original_price: "800.00",
-  new: "六成新",
-  date: "2014-11-4",
-  desc: "3年前购于美国亚马逊，附带原装皮套，皮套可以直接使用kindle的电池照明。看得少的话，一般1个月充一次电，每天看的话可能2-3周充一次电。功能完好，看英语书的绝配。"}, 
 { name: "atx750",
   title: "捷安特atx750山地变速自行车", 
   price: "800.00",
@@ -29,12 +22,20 @@ var items = [
   desc: "在专卖店买来骑行海南后，就没怎么出过远门了，Q叉前后避震。轻量改装了刹把、脚踏，安装了超级实用的泥档。仅限广州面交。"}, 
 ]
 
-$(function() {
-    for ( i = 0; i< items.length; i++) {
-        renderItem(items[i]);
-    }
-    $("#loading").hide();
-});
+
+// { name: "kindle",
+  //title: "Kindle 4 送官方带灯皮套", 
+  //price: "250.00",
+  //original_price: "800.00",
+  //new: "六成新",
+  //date: "2014-11-4",
+  //desc: "3年前购于美国亚马逊，附带原装皮套，皮套可以直接使用kindle的电池照明。看得少的话，一般1个月充一次电，每天看的话可能2-3周充一次电。功能完好，看英语书的绝配。"}, 
+//$(function() {
+    //for ( i = 0; i< items.length; i++) {
+        //renderItem(items[i]);
+    //}
+    //$("#loading").hide();
+//});
 
 function renderItem(itemJson) {
     var template = $('#itemTemplate').html();
